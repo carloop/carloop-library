98c62f762c07d1a40495180e2d32fa0030ee74b0
Julien Vanier <jvanier@gmail.com>
2016-03-20 11:10:31 -0400

    Adding GPS support

--- carloop.cpp
+++ carloop.cpp
@@ -6,10 +6,153 @@
  */
 
 #include "carloop.h"
+#include <functional>
 
-CarloopClass Carloop;
+std::function<void(char)> serialHandler;
 
-void Carloop::begin(CarloopFeatures_e features) {
-
+template<typename Config>
+Carloop<Config>::Carloop() :
+    canDriver(Config::CAN_PINS),
+    canSpeed(Config::CAN_DEFAULT_SPEED)
+{
 }
 
+template<typename Config>
+void Carloop<Config>::setCANSpeed(uint32_t canSpeed)
+{
+    this->canSpeed = canSpeed;
+}
+
+template<typename Config>
+void Carloop<Config>::begin(CarloopFeatures_e features)
+{
+    this->features = features;
+
+    if(hasCAN())
+    {
+        enableCAN();
+    }
+
+    if(hasGPS())
+    {
+        enableGPS();
+    }
+
+    if(hasBattery())
+    {
+        enableBattery();
+    }
+}
+
+template<typename Config>
+void Carloop<Config>::update()
+{
+    if(hasBattery())
+    {
+        batteryVoltage = readBattery();
+    }
+}
+
+template<typename Config>
+CANChannel &Carloop<Config>::can()
+{
+    return canDriver;
+}
+
+template<typename Config>
+TinyGPSPlus &Carloop<Config>::gps()
+{
+    return gpsDriver;
+}
+
+template<typename Config>
+float Carloop<Config>::battery()
+{
+    return batteryVoltage;
+}
+
+template<typename Config>
+void Carloop<Config>::enableCAN()
+{
+    pinMode(Config::CAN_ENABLE_PIN, OUTPUT);
+    digitalWrite(Config::CAN_ENABLE_PIN, Config::CAN_ENABLE_ACTIVE);
+    canDriver.begin(canSpeed);
+}
+
+template<typename Config>
+void Carloop<Config>::disableCAN()
+{
+    canDriver.end();
+    digitalWrite(Config::CAN_ENABLE_PIN, Config::CAN_ENABLE_INACTIVE);
+}
+
+template<typename Config>
+void Carloop<Config>::enableGPS()
+{
+    pinMode(Config::GPS_ENABLE_PIN, OUTPUT);
+    digitalWrite(Config::GPS_ENABLE_PIN, Config::GPS_ENABLE_ACTIVE);
+
+    Serial1.begin(Config::GPS_BAUD_RATE);
+
+    serialHandler = [this](char c)
+    {
+        gpsDriver.encode(c);
+    };
+}
+
+template<typename Config>
+void Carloop<Config>::disableGPS()
+{
+    digitalWrite(Config::GPS_ENABLE_PIN, Config::GPS_ENABLE_INACTIVE);
+    serialHandler = nullptr;
+}
+
+template<typename Config>
+void Carloop<Config>::enableBattery()
+{
+    pinMode(Config::BATTERY_PIN, INPUT);
+    readBattery();
+}
+
+template<typename Config>
+float Carloop<Config>::readBattery()
+{
+    static constexpr auto MAX_ANALOG_VALUE = 4096;
+    static constexpr auto MAX_ANALOG_VOLTAGE = 3.3f;
+    auto adcValue = analogRead(Config::BATTERY_PIN);
+    return adcValue * MAX_ANALOG_VOLTAGE / MAX_ANALOG_VALUE * Config::BATTERY_FACTOR;
+}
+
+template<typename Config>
+bool Carloop<Config>::hasCAN()
+{
+    return Config::FEATURES & features & CARLOOP_CAN;
+}
+
+template<typename Config>
+bool Carloop<Config>::hasGPS()
+{
+    return Config::FEATURES & features & CARLOOP_GPS;
+}
+
+template<typename Config>
+bool Carloop<Config>::hasBattery()
+{
+    return Config::FEATURES & features & CARLOOP_BATTERY;
+}
+
+// Receive bytes from the GPS
+void serialEvent1()
+{
+    while(Serial1.available())
+    {
+        char c = Serial1.read();
+        if(serialHandler)
+        {
+            serialHandler(c);
+        }
+    }
+}
+
+// Template instantiation
+template class Carloop<CarloopRevision2>;
--- carloop.h
+++ carloop.h
@@ -4,19 +4,73 @@
  *
  * Distributed under the MIT license. See LICENSE.txt for more details.
  */
+#include "application.h"
+#include "TinyGPS++.h"
 
-enum CarloopFeatures_e {
-  CARLOOP_CAN,
-  CARLOOP_GPS,
-  CARLOOP_BATTERY,
-  CARLOOP_ALL_FEATURES = CARLOOP_CAN | CARLOOP_GPS | CARLOOP_BATTERY
+enum CarloopFeatures_e
+{
+    CARLOOP_CAN = 1,
+    CARLOOP_GPS = 2,
+    CARLOOP_BATTERY = 4,
+    CARLOOP_ALL_FEATURES = CARLOOP_CAN | CARLOOP_GPS | CARLOOP_BATTERY
 };
 
-class CarloopClass {
+struct CarloopRevision2
+{
+    static constexpr auto CAN_PINS = CAN_D1_D2;
+    static constexpr uint32_t CAN_DEFAULT_SPEED = 500000;
+
+    static constexpr auto BATTERY_PIN = A1;
+    static constexpr auto BATTERY_FACTOR = 7.2f;
+
+    static constexpr auto CAN_ENABLE_PIN = D0;
+    static constexpr auto CAN_ENABLE_ACTIVE = LOW;
+    static constexpr auto CAN_ENABLE_INACTIVE = HIGH;
+
+    static constexpr auto GPS_BAUD_RATE = 9600;
+    static constexpr auto GPS_ENABLE_PIN = A0;
+    static constexpr auto GPS_ENABLE_ACTIVE = HIGH;
+    static constexpr auto GPS_ENABLE_INACTIVE = LOW;
+    // Serial cannot be configured because of dependency on the serialEvent1 handler
+    // constexpr auto &GPS_SERIAL = Serial1;
+
+    static constexpr auto FEATURES = CARLOOP_CAN | CARLOOP_GPS | CARLOOP_BATTERY;
+};
+
+template<typename Config>
+class Carloop
+{
 public:
-  Carloop();
-  void begin(CarloopFeatures_e features = CARLOOP_ALL_FEATURES);
+    Carloop();
 
+    void setCANSpeed(uint32_t canSpeed);
+    void begin(CarloopFeatures_e features = CARLOOP_ALL_FEATURES);
+
+    void update();
+
+    CANChannel &can();
+    TinyGPSPlus &gps();
+    float battery();
+
+    void enableCAN();
+    void disableCAN();
+    void enableGPS();
+    void disableGPS();
+    void enableBattery();
+
+    float readBattery();
+
+    bool hasCAN();
+    bool hasGPS();
+    bool hasBattery();
+
+private:
+    CANChannel canDriver;
+    uint32_t canSpeed;
+
+    TinyGPSPlus gpsDriver;
+
+    float batteryVoltage;
+
+    CarloopFeatures_e features;
 };
-
-extern CarloopClass Carloop;
