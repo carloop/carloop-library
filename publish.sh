a75f040bff858060e78ea8686d95163e35ead5a0
Julien Vanier <jvanier@gmail.com>
2017-06-19 22:09:16 -0400

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@
 [ -z $TRAVIS_TAG ] && exit
 
 # Check if the tag matches the library version
-[ grep -e "^version=$TRAVIS_TAG$" library.properties ] && echo "libraries.properties is not at version $TRAVIS_TAG" && exit 1
+[ -z $(grep -e "^version=$TRAVIS_TAG$" library.properties) ] && echo "libraries.properties is not at version $TRAVIS_TAG" && exit 1
 
 # Publish
 particle --no-update-check library publish
